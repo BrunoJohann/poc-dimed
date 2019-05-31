208eaa3c66152bde0e9029bc05e290a9ac346940
BrunoJohann <bruno.johann@hotmail.com>
2019-05-31 16:55:15 -0300

    Praticamente tudo foi testado

--- poc-front-dimed/src/app/component/resultado-busca/test/resultado-busca.component.spec.ts
+++ poc-front-dimed/src/app/component/resultado-busca/test/resultado-busca.component.spec.ts
@@ -3,17 +3,24 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ResultadoBuscaComponentStub as stub} from './resultado-busca.component.stub'
 
 import { ResultadoBuscaComponent } from '../resultado-busca.component';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { DetalhesItemComponent } from '../../detalhes-item/detalhes-item.component';
 
 describe('ResultadoBuscaComponent', () => {
   let component: ResultadoBuscaComponent;
   let fixture: ComponentFixture<ResultadoBuscaComponent>;
+  let detalhesItemComponent: DetalhesItemComponent;
+  let fixtureDetalhes: ComponentFixture<DetalhesItemComponent>;
   let modal: NgbModal;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResultadoBuscaComponent ],
+      declarations: [ 
+        ResultadoBuscaComponent,
+        DetalhesItemComponent
+      ],
       providers: [
+        NgbActiveModal,
         { provide: NgbModal, useClass: stub },
       ]
     })
@@ -21,6 +28,8 @@ describe('ResultadoBuscaComponent', () => {
   }));
 
   beforeEach(() => {
+    fixtureDetalhes = TestBed.createComponent(DetalhesItemComponent)
+    detalhesItemComponent = fixtureDetalhes.componentInstance;
     modal = TestBed.get(NgbModal);
     fixture = TestBed.createComponent(ResultadoBuscaComponent);
     component = fixture.componentInstance;
@@ -31,13 +40,21 @@ describe('ResultadoBuscaComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  xdescribe('', () => {
+  describe('Dado que [buscaProduto] tenha sido chamada>>>>', () => {
     beforeEach(() => {
-      spyOn(modal, 'open').and.callFake(() => { })
+      spyOn(modal, 'open').and.callFake(() => { 
+        return fixtureDetalhes
+      }) 
+      spyOn(detalhesItemComponent, 'setItemFinal')
+      component.openModal( stub.mockItemFinal() );
     });
 
-    it('', () => {
-      expect(component.openModal( stub.mockItemFinal() ))
+    it('Então deve ser chamado a função [open]', () => {
+      expect(modal.open).toHaveBeenCalled();
+    });
+
+    it('Então deve ser chamado a função [setItemFinal]', () => {
+      expect(detalhesItemComponent.setItemFinal).toHaveBeenCalled();
     })
   });
 });
